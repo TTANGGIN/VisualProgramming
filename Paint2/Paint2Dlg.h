163ab139f06cd2505df076d0eccded2262561f2b
TTANGGIN <knm6175@gmail.com>
2021-09-24 13:48:53 +0900

    Paint2 완성

---
+++ Paint2/Paint2Dlg.h
@@ -0,0 +1,38 @@
+﻿
+// Paint2Dlg.h: 헤더 파일
+//
+
+#pragma once
+
+
+// CPaint2Dlg 대화 상자
+class CPaint2Dlg : public CDialogEx
+{
+// 생성입니다.
+public:
+	CPaint2Dlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_PAINT2_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	CScrollBar m_ctlHori;
+//	afx_msg void OnNMThemeChangedScrHori(NMHDR* pNMHDR, LRESULT* pResult);
+	afx_msg void OnHScroll(UINT nSBCode, UINT nPos, CScrollBar* pScrollBar);
+};
